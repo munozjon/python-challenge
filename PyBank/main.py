7f355bece818a563473c08fc335352d5bcaa0b2b
munozjon <munozjon98@gmail.com>
2024-03-18 22:50:25 -0400

    Added code to append positive and negative changes to lists and returning largest and smallest profits

--- PyBank/main.py
+++ PyBank/main.py
@@ -5,7 +5,7 @@ import csv
 # Variables to calculate
 total_months = 0
 net_total = 0
-avg_changes = 0
+changes_over_period = 0
 greatest_increase = []
 greatest_decrease = []
 
@@ -25,15 +25,20 @@ with open(csvpath) as csvfile:
     # Loop through each row of data
     for row in csvreader:
 
-        # Add 1 to keep count of each month
+        # Add 1 to keep count of each month (each row is a month)
         total_months += 1
 
         # Add/subtract the profit/loss column to value by converting string to float
         net_total += float(row[1])
 
-
-        # Print row
-        # print(row)
+        # Check if value is positive or negative, then append value to respective list
+        if int(row[1]) > 0:
+            greatest_increase.append(int(row[1]))
+        else:
+            greatest_decrease.append(int(row[1]))
 
 print(f"Total months: {total_months}")
 print(f"Net total: {net_total}")
+
+print(max(greatest_increase))
+print(min(greatest_decrease))
